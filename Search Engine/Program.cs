683e19f4c34fcec699e1409f76ff4c6c1948f07d
Shayanlion <Shayanlion25@gmail.com>
2018-08-09 17:48:49 -0400

    Added functionality for retrieveing paper data from altmetric explorer

--- Search Engine/Program.cs
+++ Search Engine/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -10,14 +12,37 @@ namespace Search_Engine
     {
         static void Main(string[] args)
         {
-            //Dictionary<string, string> articles = Search
+            Dictionary<string, string> articles = Search("cancer").Result;
+            
         }
 
-        public async Task<Dictionary<string, string>> Search(string searchTerm)
+        private static async Task<Dictionary<string, string>> Search(string searchTerm)
         {
-            WebRequest request = WebRequest.Create("https://www.altmetric.com/explorer.login");
-            WebResponse response = await request.GetResponseAsync();
-            //Match matchObject = Regex.Match()
+            HttpClient client = new HttpClient();
+            var loginPage = await client.GetStringAsync("https://www.altmetric.com/explorer/login");
+            
+            Match matchObject = Regex.Match(loginPage, @"name=""authenticity_token"" value=""(?<key>.+)""");
+            string token = string.Empty;
+            if(matchObject.Success) token = matchObject.Groups["key"].Value;
+            
+            Dictionary<string, string> formFields = new Dictionary<string, string>()
+            {
+                {"email", "bigdata@stemfellowship.org"},
+                {"password", "bigdatachallenge"},
+                {"authenticity_token", token},
+                {"commit", "Sign in"}
+            };
+
+            FormUrlEncodedContent content = new FormUrlEncodedContent(formFields);
+            var response = await client.PostAsync("https://www.altmetric.com/explorer/login", content);
+            var searchResults =
+                await client.GetStringAsync("https://www.altmetric.com/explorer/json_data/research_outputs?q=" + searchTerm +
+                                      "&scope=all");
+            
+            Console.WriteLine(searchResults);
+            
+            client.Dispose();
+            
             Dictionary<string, string> dict = new Dictionary<string, string>();
             return dict;
         }
