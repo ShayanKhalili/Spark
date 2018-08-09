cc6ed2ecd9f04342178c416a6762c0a78408c456
Shayanlion <shayanlion25@gmail.com>
2018-08-09 04:09:33 -0400

    Created search engine class

---
+++ Search Engine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Search_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> articles = Search
+        }
+
+        public async Task<Dictionary<string, string>> Search(string searchTerm)
+        {
+            WebRequest request = WebRequest.Create("https://www.altmetric.com/explorer.login");
+            WebResponse response = await request.GetResponseAsync();
+            //Match matchObject = Regex.Match()
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            return dict;
+        }
+    }
+    
+}
